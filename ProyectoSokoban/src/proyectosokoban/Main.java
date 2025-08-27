8aa06788b7cb4ae66379490f2c758f2390d41038
ftMadrid <fernandomadridcv04@gmail.com>
2025-08-26 19:07:53 -0600

    Proyecto creado

---
+++ ProyectoSokoban/src/proyectosokoban/Main.java
@@ -0,0 +1,11 @@
+package proyectosokoban;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Fernando has arrived babe!");
+        
+    }
+    
+}
