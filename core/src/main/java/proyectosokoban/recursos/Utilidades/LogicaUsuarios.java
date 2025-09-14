d6c06c430e2dbbe7bcdab090ebaa10a54121198f
ftMadrid <fernandomadridcv04@gmail.com>
2025-09-14 01:34:06 -0600

    creados niveles 1-4

--- core/src/main/java/proyectosokoban/recursos/Utilidades/LogicaUsuarios.java
+++ core/src/main/java/proyectosokoban/recursos/Utilidades/LogicaUsuarios.java
@@ -1,13 +1,5 @@
+package proyectosokoban.recursos.Utilidades;
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author saidn
- */
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
