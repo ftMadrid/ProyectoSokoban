25af6f445a5752870b73b2e946469064706a5067
ftMadrid <fernandomadridcv04@gmail.com>
2025-09-22 09:45:59 -0600

    viva cholomaaaaa

--- core/src/main/java/proyectosokoban/recursos/Screens/GameScreen.java
+++ core/src/main/java/proyectosokoban/recursos/Screens/GameScreen.java
@@ -14,7 +14,6 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
